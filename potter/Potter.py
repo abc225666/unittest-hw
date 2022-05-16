738770cd3f82b598b197269acfa942511bff6447
Ting-Rui, Wang <qwe50120@gmail.com>
2022-05-16 14:14:15 +0000

    Init: framework

---
+++ potter/Potter.py
@@ -0,0 +1,11 @@
+class Potter:
+    def __init__(self):
+        self.book_cart = {"book1": 0, "book2": 0, "book3": 0, "book4": 0, "book5": 0}
+
+    def add_cart(self, books):
+        # books type is {"book1": amount, "book2": amount....}
+        pass
+
+    def get_price(self):
+        # get final price with discount
+        pass
